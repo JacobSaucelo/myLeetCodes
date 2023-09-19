d7ed99f33a9a5d9b8a7decf1288c91d5f0750bb4
csthesisgroup <jacobsaucelo32@gmail.com>
2023-09-19 14:50:25 +0800

    k frequent elements problem

---
+++ kFrequentElements/app.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+
+var topKFrequent = function (nums, k) {};
+
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
+// console.log(topKFrequent([1], 1));
+
+// Example 1:
+
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
