ee48d843c38aa1805b67518f355f92cc5d72d2bd
csthesisgroup <jacobsaucelo32@gmail.com>
2023-09-19 13:55:50 +0800

    groupAnagrams problem

---
+++ groupAnagrams/app.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {};
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+console.log(groupAnagrams([""]));
+console.log(groupAnagrams(["a"]));
+
+// Example 1:
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// Example 2:
+
+// Input: strs = [""]
+// Output: [[""]]
+
+// Example 3:
+
+// Input: strs = ["a"]
+// Output: [["a"]]
