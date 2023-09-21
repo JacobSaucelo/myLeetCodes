0c5cc0b7582ff0458b8b1317437755f74ed0efc5
csthesisgroup <jacobsaucelo32@gmail.com>
2023-09-21 13:29:58 +0800

    finished ProductOfArrayExceptSelf

--- productOfArrayExceptSelf/app.js
+++ productOfArrayExceptSelf/app.js
@@ -3,36 +3,28 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-  const pre = [];
+  let preOrder = [];
   let post = 1;
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i - 1] === undefined) {
-      pre[i] = nums[i];
-    } else {
-      pre[i] = pre[i - 1] * nums[i];
-    }
-
-    // let value = !nums[i - 1] ? nums[i] ----: pre[i - 1] * nums[i];
-    // pre[i] = value;
+    let value = nums[i - 1] === undefined ? nums[i] : nums[i] * preOrder[i - 1];
+    preOrder[i] = value;
   }
 
   for (let j = nums.length - 1; j >= 0; j--) {
-    if (nums[j + 1] === undefined) {
-      console.log("pre", pre, "post", post, "nums", nums);
-      pre[j] = pre[j - 1];
+    if (preOrder[j + 1] === undefined) {
+      preOrder[j] = preOrder[j - 1];
       post = nums[j];
     } else {
-      console.log("pre", pre, "post", post, "nums", nums);
-      pre[j] = (pre[j - 1] === undefined ? 1 : pre[j - 1]) * post;
+      preOrder[j] =
+        (preOrder[j - 1] === undefined ? 1 : preOrder[j - 1]) * post;
       post = post * nums[j];
     }
   }
 
-  return pre;
+  return preOrder;
 };
-
-console.log(productExceptSelf([1, 2, 3, 4]));
+// console.log(productExceptSelf([1, 2, 3, 4]));
 console.log(productExceptSelf([-1, 1, 0, -3, 3]));
 
 // Example 1:
