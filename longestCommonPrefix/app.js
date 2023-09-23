5e1eeeb0d5fa8b666be7e3ce71835514e7b6e531
csthesisgroup <jacobsaucelo32@gmail.com>
2023-09-23 14:02:16 +0800

    longestCommonPrefix

---
+++ longestCommonPrefix/app.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {};
+
+// console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
